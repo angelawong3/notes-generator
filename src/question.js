bc61c1779321481d18410a7071a3ef9f0e94fc49
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-16 15:10:17 +0100

    add appointment question

--- src/question.js
+++ src/question.js
@@ -26,3 +26,63 @@ const billQuestions = [
     clearable: true,
   },
 ];
+
+const appointmentQuestions = [
+  {
+    type: "input",
+    message: "Please enter the title of the appointment",
+    name: "title",
+    // validate: () => {},
+  },
+  {
+    type: "list",
+    message: "Please select the type of appointment:",
+    name: "type",
+    choices: [
+      {
+        name: "Doctor",
+        value: "doctor",
+        short: "Doctor",
+      },
+      {
+        name: "Dentist",
+        value: "dentist",
+        short: "Dentist",
+      },
+      {
+        name: "Hair Dresser",
+        value: "hairDresser",
+        short: "Hair",
+      },
+      {
+        name: "Meeting",
+        value: "meeting",
+        short: "Meeting",
+      },
+    ],
+    // validate: () => {},
+  },
+  {
+    type: "date",
+    name: "date",
+    message: "Please enter the appointment date:",
+    default: new Date(), // default to today's date
+    format: { month: "short" },
+    clearable: true,
+  },
+];
+
+const appointmentAttendeesQuestions = [
+  {
+    type: "input",
+    message: "Please enter the full name of attendee:",
+    name: "attendees",
+    // validate: () => {},
+  },
+  {
+    type: "confirm",
+    message: "Would you like to add another attendee?",
+    name: "addAnotherAttendee",
+    // validate: () => {},
+  },
+];
